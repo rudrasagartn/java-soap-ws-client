f773557919316dbc330c77964c39ce19327db14d
Rudrasagar <rudrasagar.tn@gmail.com>
2019-02-22 19:02:13 +0530

    initial develop branch commit

---
+++ src/com/webservice/ISoapService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.webservice;
+
+/**
+ * @author rudrasagar.tn
+ *
+ */
+public interface ISoapService {
+	
+	String WEB_SERVICE_URL ="http://www.dneonline.com/calculator.asmx?wsdl"; 
+	String NAME_SPACE_URI="http://tempuri.org/";
+	String LOCAL_PART_NAME="Calculator";
+
+}
